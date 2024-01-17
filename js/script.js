7b3cc8635737bb2214682128c4fac5e922039fe9
At1set <114298791+At1set@users.noreply.github.com>
2024-01-18 00:57:25 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  let music = new Audio("../src/music/ogBuda.mp3")
+  let flag = false
+  setTimeout(() => {
+    flag = true
+  }, 1000);
+
+  // document.addEventListener("mousemove", function () {
+  //   if (flag) {
+  //     music.play()
+  //   }
+  // })
+}
